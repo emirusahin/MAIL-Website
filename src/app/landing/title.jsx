c967db62c6cf1e6fa723136d9a8579c52f8a447e
Jacob <Jacob.leader06@gmail.com>
2024-10-01 11:59:42 -0400

    feat: reactive title

--- src/app/landing/title.jsx
+++ src/app/landing/title.jsx
@@ -1,7 +1,36 @@
+'use client';
+import { useState, useEffect } from 'react';
+
 export default function Title() {
-    return (
-        <h1 className="font-Lora text-white text-9xl leading-none text-center p-0">We are<br/>McGill AI Lab</h1>
-        // <h1 className="font-Lora text-white text-5xl leading-none text-center p-0">We are<br/>McGill<br/>AI Lab</h1>
-        // <h1 className="font-Lora text-white text-5xl leading-none text-center p-0">McGill AI Lab</h1>
-    );
+    const [windowWidth, setWindowWidth] = useState(null);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const handleResize = () => setWindowWidth(window.innerWidth);
+            setWindowWidth(window.innerWidth);
+            window.addEventListener('resize', handleResize);
+
+            // Cleanup event listener on component unmount
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    // before client mount)
+    if (windowWidth === null) {
+        return null;
+    }
+
+    if (windowWidth > 500) {
+        return (
+            <h1 className="font-Lora text-white text-9xl leading-none text-center p-0">
+                We are<br />McGill AI Lab
+            </h1>
+        );
+    } else { // Mobile
+        return (
+            <h1 className="font-Lora text-white text-7xl leading-none text-center p-0">
+                McGill AI Lab
+            </h1>
+        );
+    }
 }
