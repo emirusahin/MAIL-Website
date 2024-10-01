a40ba581c774c1597aa8d2b23062ac3dd3af78eb
Jacob <Jacob.leader06@gmail.com>
2024-09-30 21:58:40 -0400

    feat: fall off on click

--- src/app/landing/about.jsx
+++ src/app/landing/about.jsx
@@ -1,10 +1,19 @@
+'use client';
+import { useState } from 'react';
+
+
 export default function About(){
+    const  [animation, setAnimation] = useState('');
+
     const text = `
         McGill AI Lab is a student-led initiative at McGill University. Our goal is to apply our theoretical knowledge to wide variety of real world problems (or musings). From identifying pain biomarkers in EEG data to predicting McGill students’ academic major based on their clothing style, our work spans diverse research areas. 
     `;
 
+    const handleClick = () => {
+        setAnimation('animate__animated animate__hinge')
+    };
     return (
-        <div className="text-md">{ text }</div>
+        <div onClick={handleClick} className={`text-md ${animation}`}>{ text }</div>
     );
 
 }
