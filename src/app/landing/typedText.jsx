715ab14fa5a654916c25a1b6da963ce60e202de3
Jacob <Jacob.leader06@gmail.com>
2024-10-03 21:16:30 -0400

    feat: typed text title

---
+++ src/app/landing/typedText.jsx
@@ -0,0 +1,40 @@
+'use client';
+import Typed from 'typed.js';
+import React from 'react';
+import useWindowSize from '../functions/useWindowWidth';
+
+export default function TypedText() {
+    const el = React.useRef(null);
+    const { width: windowWidth } = useWindowSize();
+    
+    React.useEffect(() => {
+      const typed = new Typed(el.current, {
+        strings: ['We are<br/>the best', 'We are<br/>McGill AI Lab^1500'],
+        // smartBackspace: true, // Default value
+        typeSpeed: 60,
+        backSpeed: 100,
+        backDelay: 1500,
+        onComplete: function(self) { self.cursor.remove() }
+      });
+  
+      return () => {
+        typed.destroy();
+      };
+    }, []);
+  
+    if (windowWidth > 500) {
+        return (
+        <div className="font-lora text-white text-9xl leading-none text-center p-0">
+            <span ref={el} />
+        </div>
+        );
+    } else {
+        return (
+            <div className={`font-lora text-white leading-none text-center p-0 ${
+                    windowWidth > 500 ? 'text-9xl' : 'text-7xl'
+                }`}>
+                <span ref={el} />
+            </div>
+            );
+    }
+}
