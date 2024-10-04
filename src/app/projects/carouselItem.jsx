6bbd6585b11e32d7e65f1b4633986940caa3a0ab
Jacob <Jacob.leader06@gmail.com>
2024-10-04 00:38:18 -0400

    fix: change hover transition and cursor

--- src/app/projects/carouselItem.jsx
+++ src/app/projects/carouselItem.jsx
@@ -2,9 +2,9 @@
 import '../../styles/globals.css';
 
 
-export default function AltCarouselItem({ title, date, image }) {
+export default function CarouselItem({ title, date, image }) {
     return (
-        <div className="min-w-[25vw] h-auto flex flex-col items-start transition-transform duration-300 transform hover:rotate-y-12 hover:scale-105">
+        <div className="min-w-[25vw] h-auto flex flex-col items-start cursor-pointer transition-transform duration-300 transform hover:rotate-y-12 hover:scale-105 ">
             <div className="flex justify-center overflow-hidden w-full aspect-[361/261]">
                 <img src={image} className="w-full h-full object-cover" alt={title} />
             </div>
