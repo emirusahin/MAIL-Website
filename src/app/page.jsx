bc84c076f45f97f764d88e334a6bd9430bc36dec
Jacob <Jacob.leader06@gmail.com>
2024-10-03 21:16:57 -0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,26 @@
 // pages/index.js
-
+'use client';
 import ParticlesBackground from '../components/TSparticles';
 import ProjectsButton from '../components/projectsButton.jsx';
 import Title from './landing/title.jsx';
 import About from './landing/about.jsx';
 import Carousel from './projects/carousel';
+import TypedText from './landing/typedText';
 import './globals.css';
+import useWindowSize from './functions/useWindowWidth';
 
 export default function Home() {
+  const { width: windowWidth } = useWindowSize();
+
   return (
     <ParticlesBackground>
       <div className="flex flex-col items-center h-screen relative pt-40">
         <div className="w-1/2 text-center text-white">
-          <Title/>
-          <About/>
-        </div>
+          <TypedText/>
+          {windowWidth > 500 && <About />}
+          </div>
       </div>
+      
       <Carousel/>
     </ParticlesBackground>
   );
