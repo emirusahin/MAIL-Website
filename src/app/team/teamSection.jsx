f23df765c313f3e46a2f52b480647fbab0ebf4f7
Jacob <Jacob.leader06@gmail.com>
2024-10-07 22:02:43 -0400

    change style

--- src/app/team/teamSection.jsx
+++ src/app/team/teamSection.jsx
@@ -1,77 +1,28 @@
 import React from 'react';
 import teamMembers from '../../data/teamMembers.json';
-import Link from 'next/link';
-
-
-function ArrowRow({teamMembers}) {
-    return(
-        <div className='flex flex-col justify-center items-center w-full'>
-            {teamMembers.map((member, index) => (
-                <div key={index} className="rounded-lg p-1 w-28 cursor-pointer">
-                    <img 
-                        src={member.image} 
-                        alt={`Team Member ${index + 1}`} 
-                        className="w-full h-24 object-cover rounded-md" 
-                    />
-                </div>
-            ))}
-        </div>
-    );
-}
 
 export default function TeamSection() {
-      return (
-        <>
-        <Link href="/team" className="text-white text-9xl">CLICK HERE</Link>
-        <div className="flex flex-row items-center my-10">
-            <div className="flex flex-col justify-center pl-8 w-1/4">
-                <h1 className="font-Inter text-white sm:text-5xl lg:text-10xl text-center">The<br />Team</h1>
+      return (        
+        <a href="/team" className="flex flex-row items-center my-10 cursor-default">
+            <div className="flex flex-col justify-center pl-8 w-1/2 cursor-default">
+                <h1 className="font-Inter text-white sm:text-5xl lg:text-10xl text-center cursor-pointer">
+                The<br />Team
+                </h1>
             </div>
-            <div onClick="TODO" className='flex flex-row w-3/4 pl-52'>
-                <div className='flex flex-row justify-start items-center w-full'>
-                    {teamMembers.slice(0, 4).map((member, index) => (
-                        <div key={index} className="bg-gray-900 rounded-lg p-1 w-28">
-                            <img 
-                                src={member.image} 
-                                className="w-full h-24 object-cover rounded-md cursor-pointer" 
-                            />
-                        </div>
-                    ))}
-                </div>
-                    <ArrowRow teamMembers={teamMembers.slice(4, 10)} />
-                    <ArrowRow teamMembers={teamMembers.slice(10, 14)} />
-                    <ArrowRow teamMembers={teamMembers.slice(14, 16)} />
-                <div className='flex flex-col justify-center items-center w-full'>
-                    <div className="rounded-lg p-1 w-28">
-                        <img 
-                            src={teamMembers[6].image} 
-                            className="w-full h-24 object-cover rounded-md cursor-pointer" 
-                        />
+
+            <div className='grid grid-cols-3 grid-rows-3 w-1/3 cursor-default transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl'>
+                {teamMembers.slice(0, 9).map((member) => (
+                    <div 
+                    className="bg-gray-800 rounded-lg p-4 shadow-lg  cursor-pointer"
+                    >
+                    <img 
+                        src={member.image} 
+                        className="w-36 h-36 object-cover rounded-md cursor-pointer" 
+                        alt={`Photo of ${member.name}`}
+                    />
                     </div>
-                </div>
+                ))}
             </div>
-        </div>
-        </>
+        </a>
     );
 }
-
-
-
-// <>
-        //     <h1 className="font-Inter text-white sm:text-5xl lg:text-7xl pl-8 pb-4">The Team</h1>
-        //     <div className="flex flex-col items-center my-10 pl-4">
-        //         {/* Set horizontal scrolling with overflow-x-auto */}
-        //         <div className="overflow-x-auto whitespace-nowrap max-w-full">
-        //             <div className="inline-flex">
-        //                 {teamMembers.map((member, index) => (
-        //                     <div key={index} className="bg-gray-800 text-white rounded-lg p-4 mx-2 w-80">
-        //                         <img src={member.image} alt={member.name} className="w-full h-64 object-cover rounded-lg mb-4" />
-        //                         <h3 className="text-xl font-semibold">{member.name}</h3>
-        //                         <p className="text-sm">{member.year} {member.major}</p>
-        //                         <p className="text-sm">{member.role}</p>
-        //                     </div>
-        //                 ))}
-        //             </div>
-        //         </div>
-        //     </div>
-        // </>
