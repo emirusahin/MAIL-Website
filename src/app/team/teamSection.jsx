a27114a8150d742a5f953ec51d0bf140d597a533
Jacob <Jacob.leader06@gmail.com>
2024-10-07 22:36:55 -0400

    fix: key for all mapped elements

--- src/app/team/teamSection.jsx
+++ src/app/team/teamSection.jsx
@@ -11,18 +11,20 @@ export default function TeamSection() {
             </div>
 
             <div className='grid grid-cols-3 grid-rows-3 w-1/3 cursor-default transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl'>
-                {teamMembers.slice(0, 9).map((member) => (
+                {teamMembers.slice(0, 9).map((member, index) => (
                     <div 
-                    className="bg-gray-800 rounded-lg p-4 shadow-lg  cursor-pointer"
+                        key={index}
+                        className="bg-gray-800 rounded-lg p-4 shadow-lg cursor-pointer"
                     >
-                    <img 
-                        src={member.image} 
-                        className="w-36 h-36 object-cover rounded-md cursor-pointer" 
-                        alt={`Photo of ${member.name}`}
-                    />
+                        <img 
+                            src={member.image} 
+                            className="w-36 h-36 object-cover rounded-md cursor-pointer" 
+                            alt={`Photo of ${member.name}`}
+                        />
                     </div>
                 ))}
             </div>
+
         </a>
     );
 }
