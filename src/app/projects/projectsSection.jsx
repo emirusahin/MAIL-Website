0c024aefbd9b0734595fc532ae502282db79b3d7
Jacob <Jacob.leader06@gmail.com>
2024-10-07 22:02:29 -0400

    dynamic routing

--- src/app/projects/projectsSection.jsx
+++ src/app/projects/projectsSection.jsx
@@ -1,18 +1,20 @@
 import CarouselItem from "./carouselItem";
+import projectsData from '../../data/projectsData.json';
 
 export default function ProjectsSection() {
     return (
         <>
             <h1 className="font-Inter text-white sm:text-5xl lg:text-7xl pl-8 pb-4">Projects</h1>
             <div className="carousel bg-none rounded-box w-full space-x-4 p-4 pl-8 overflow-auto">
-                <CarouselItem title="Student Major Predictor" date="Sept 12. 2024" image="/images/major-predictor-cover.png"/>
-                <CarouselItem title="Student Major Predictor" date="Sept 12. 2024" image="/images/major-predictor-cover.png"/>
-                <CarouselItem title="Student Major Predictor" date="Sept 12. 2024" image="/images/major-predictor-cover.png"/>
-                <CarouselItem title="Student Major Predictor" date="Sept 12. 2024" image="/images/major-predictor-cover.png"/>
-                <CarouselItem title="Student Major Predictor" date="Sept 12. 2024" image="/images/major-predictor-cover.png"/>
-                <CarouselItem title="Student Major Predictor" date="Sept 12. 2024" image="/images/major-predictor-cover.png"/>
-                <CarouselItem title="Student Major Predictor" date="Sept 12. 2024" image="/images/major-predictor-cover.png"/>
+                {projectsData.map((project) => makeProjectItem(project))}
             </div> 
         </>
     );
+}
+
+
+function makeProjectItem(project){
+    return (
+        <CarouselItem title={project.title} date={project.date} image={project.image} />
+    )
 }
