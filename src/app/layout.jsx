b856ba800fcd11dec899b03ea254dc60f92d5455
Jacob <Jacob.leader06@gmail.com>
2024-10-07 22:31:29 -0400

    Integrate PostHog

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,23 @@
 import 'animate.css';
 import '../styles/globals.css';
-// import { NextUIProvider } from '@nextui-org/react'; // Import NextUIProvider
+import { CSPostHogProvider } from './providers';
 import Heading from './header/header';
 import Navbar from './header/header';
 
 export default function Layout({ children }) {
+  
   return (
     <html lang="en" className='bg-black'>
-        <body>
-          <div className='bg-blue'>
-            <main>
-            <Navbar/>
-              {children}
-            </main>
-          </div>
-        </body>
+        <CSPostHogProvider>
+          <body>
+            <div className='bg-blue'>
+              <main>
+              <Navbar/>
+                {children}
+              </main>
+            </div>
+          </body>
+        </CSPostHogProvider>
     </html>
 
   );
