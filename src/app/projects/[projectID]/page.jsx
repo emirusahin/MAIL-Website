c5b555b2c721e33ba3ff7c3ed2c99f7c1a6e22b7
Jacob <Jacob.leader06@gmail.com>
2024-10-10 22:39:24 -0400

    remove log

--- src/app/projects/[projectID]/page.jsx
+++ src/app/projects/[projectID]/page.jsx
@@ -2,7 +2,6 @@ import projectsData from '../../../data/projectsData.json' assert { type: 'json'
 
 export default async function ProjectPage({ params }) {
     const projectDetails = await getProjectDetails(params.projectID);
-    console.log(projectDetails);
     return (
         <div className='flex flex-col items-center justify-center pt-4'>
             <div className="text-white text-7xl pb-4">{projectDetails.title}</div>
