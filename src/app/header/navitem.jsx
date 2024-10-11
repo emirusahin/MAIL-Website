6dd9057c131a6546e3892ad5cae693c1ca89b44f
Jacob <Jacob.leader06@gmail.com>
2024-10-10 23:41:01 -0400

    handles navigation based on current page

--- src/app/header/navitem.jsx
+++ src/app/header/navitem.jsx
@@ -1,15 +1,32 @@
 'use client';
-
-const scrollToSection = (id) => {
-    const element = document.getElementById(id);
-    element.scrollIntoView({ behavior: "smooth" });
-};
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function Navitem({ label, id, color }) {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const handleNavigation = (id) => {
+        if (pathname === "/") { // On home page
+            const element = document.getElementById(id);
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+        else if (pathname === "/" + id){ // Current page
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+        else { // Other page
+            router.push('/' + id);
+            console.log(id)
+        }
+    };
+
     return (
         <li>
             <button 
-                onClick={() => {scrollToSection(id);}} className={`text-xl text-${color} lg:px-5`}>
+                onClick={() => handleNavigation(id)} 
+                className={`text-xl text-${color} lg:px-5 cursor-pointer`}
+            >
                 {label}
             </button>
         </li>
