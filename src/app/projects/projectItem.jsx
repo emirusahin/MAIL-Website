90e8f016cdb013fe01325495066cb2d254019653
Jacob <Jacob.leader06@gmail.com>
2024-10-21 19:11:51 -0400

    add clickable toggle

--- src/app/projects/projectItem.jsx
+++ src/app/projects/projectItem.jsx
@@ -3,28 +3,51 @@ import '../../styles/globals.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function ProjectItem({ id, title, date, image }) {
-    const link = `/projects/${convertTitleToSlug(title)}`; // Use slug for link if desired
+export default function ProjectItem({ id, title, date, image, clickable }) {
+    const link = `/projects/${convertTitleToSlug(title)}`;
 
     return (
-        <Link href={link} aria-label={`View project: ${title}`} className="group">
-            <li key={title}>
-                <div className="min-w-[70vw] lg:min-w-[25vw] h-auto flex flex-col items-start cursor-pointer transition-transform duration-300 transform hover:scale-105">
-                    <div className="flex justify-center overflow-hidden w-full aspect-[361/261] relative">
-                        <Image
-                            src={image}
-                            alt={title}
-                            className="object-cover"
-                            fill={true}
-                        />
+       <>
+        {clickable ? (
+            <Link href={link} aria-label={`View project: ${title}`} className="group">
+                <li key={title}>
+                    <div className="min-w-[70vw] lg:min-w-[25vw] h-auto flex flex-col items-start cursor-pointer transition-transform duration-300 transform hover:scale-105">
+                        <div className="flex justify-center overflow-hidden w-full aspect-[361/261] relative">
+                            <Image
+                                src={image}
+                                alt={title}
+                                className="object-cover"
+                                fill={true}
+                            />
+                        </div>
+                        <div className="flex flex-col justify-end items-start w-full pt-4">
+                            <div className="text-gray text-xs sm:text-xs md:text-sm">{date || 'Date not available'}</div>
+                            <div className="text-white text-xl md:text-2xl lg:text-3xl pt-2">{title || 'Untitled'}</div>
+                        </div>
                     </div>
-                    <div className="flex flex-col justify-end items-start w-full pt-4">
-                        <div className="text-gray text-xs sm:text-xs md:text-sm">{date || 'Date not available'}</div>
-                        <div className="text-white text-xl md:text-2xl lg:text-3xl pt-2">{title || 'Untitled'}</div>
+                </li>
+            </Link>
+        ) : (
+            <div className="group">
+                <li key={title}>
+                    <div className="min-w-[70vw] lg:min-w-[25vw] h-auto flex flex-col items-start cursor-default opacity-60">
+                        <div className="flex justify-center overflow-hidden w-full aspect-[361/261] relative">
+                            <Image
+                                src={image}
+                                alt={title}
+                                className="object-cover"
+                                fill={true}
+                            />
+                        </div>
+                        <div className="flex flex-col justify-end items-start w-full pt-4">
+                            <div className="text-gray text-xs sm:text-xs md:text-sm">{date || 'Date not available'}</div>
+                            <div className="text-white text-xl md:text-2xl lg:text-3xl pt-2">{title || 'Untitled'}</div>
+                        </div>
                     </div>
-                </div>
-            </li>
-        </Link>
+                </li>
+            </div>
+        )}
+       </>
     );
 }
 
