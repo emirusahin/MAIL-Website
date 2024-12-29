d75895837e0923148c9932f0aaca604a189cf3d8
Jacob <Jacob.leader06@gmail.com>
2024-12-29 12:19:14 -1000

    add alt and fix layout

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -5,19 +5,21 @@ export default function Team() {
     return (
         <>
             <h1 className="font-Inter text-black sm:text-5xl lg:text-8xl text-center">Execs</h1>
-            <div className="flex flex-col items-center my-10">
-                <div className="flex flex-wrap justify-center gap-4">
-                    {teamMembers.map((member, index) => (
+            
+            {/* Outer div centers everything */}
+            <div className="flex justify-center my-10">
+                {/* Inner div aligns columns to the left */}
+                <div className="grid grid-cols-4 gap-4">
+                    {teamMembers.slice(0, 9).map((member, index) => (
                         <div 
                             key={index} 
                             className="bg-gray-800 text-black rounded-lg p-1 w-80">
                             <div className="relative w-full h-64 mb-4">
                                 <Image
                                     src={member.image} 
-                                    alt={member.name} 
+                                    alt={`Photo of ${member.name}, McGill AI Lab executive who has the role of ${member.role}`}
                                     className="object-cover rounded-lg" 
                                     layout="fill" 
-                                    // objectFit="cover" 
                                     priority={index < 9}
                                 />
                             </div>
